5bc77f14e27897c1493129d3ce78876e6bc76241
emersonweslley <ew.guedes@yahoo.com.br>
2019-09-07 20:27:00 -0300

    Add files via upload

---
+++ Debug/src/Timer.d
@@ -0,0 +1,70 @@
+src/Timer.o: ../src/Timer.c \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/drivers/pwr/adi_pwr.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/adi_callback.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/adi_processor.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_cdef.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_def.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/adi_cio_macros.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_cdef.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_device.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_typedefs.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_typedefs.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_device.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_typedefs.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/core_cm3.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/cmsis_version.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/cmsis_gcc.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/mpu_armv7.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/system_ADuCM3029.h \
+ ../src/Timer.h \
+ C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/common.h
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/drivers/pwr/adi_pwr.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/adi_callback.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/adi_processor.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_cdef.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_def.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/adi_cio_macros.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_cdef.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_device.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/ADuCM3029_typedefs.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_typedefs.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_device.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/sys/ADuCM302x_typedefs.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/core_cm3.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/cmsis_version.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/cmsis_compiler.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/cmsis_gcc.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/ARM/CMSIS/5.4.0/CMSIS/Core/Include/mpu_armv7.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/system_ADuCM3029.h:
+
+../src/Timer.h:
+
+C:/Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.8.0/ARM/packs/AnalogDevices/ADuCM302x_DFP/3.2.0/Include/common.h:
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/Communication.c \
+../src/PinMux.c \
+../src/Test.c \
+../src/Timer.c \
+../src/main.c 
+
+SRC_OBJS += \
+./src/Communication.o \
+./src/PinMux.o \
+./src/Test.o \
+./src/Timer.o \
+./src/main.o 
+
+C_DEPS += \
+./src/Communication.d \
+./src/PinMux.d \
+./src/Test.d \
+./src/Timer.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
+	arm-none-eabi-gcc -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-71f1f464465ef79a7e7b0ac7348f7f7f.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
